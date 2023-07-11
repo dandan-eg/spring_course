60f450b0fe60328ec9fe8d085a846057eeebd3cb
Yanis Adekalom <yanis971.60@gmail.com>
2023-07-11 09:23:13 +0200

    HOTFIX : Demo PagingAngSortingRepository removed

--- src/main/java/fr/dawan/jpahibernate/entities/basic/repo/B_FooRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.dawan.jpahibernate.entities.basic.repo;
-
-import fr.dawan.jpahibernate.entities.basic.models.B_Foo;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface B_FooRepository extends PagingAndSortingRepository<B_Foo, Long> {
-}
